038490a7f4623a308081763e3ba18ffb2fedffef
Eugenio Levorato <eugeniolevorato5@gmail.com>
2024-09-02 06:13:22 -0400

    Modifica codice per la verifica socket

--- verificaSocket.py
+++ verificaSocket.py
@@ -1,23 +1,45 @@
 import socket as so
+from prettytable import PrettyTable
 
-target_ip = "?????"#Inserire appena possibile l'ip del webserver di cisco packet tracer
+target_ip = input("Inserire l'ip del web server interessato: \n")
 portrange = input("Inserisci un port range (es 5-200): ")
-lowport = 5
-highport = 200
+lowport = 1
+highport = 1024
 if "-" in portrange:
     lowport = int(portrange.split('-')[0])
     highport = int(portrange.split('-')[1])
 
+porte_aperte = []
+porte_chiuse = []
+
 # Scansione delle porte
-def port_scan(ip, lowport, highport):
+def verificaSocket(ip, lowport, highport):
     for port in range(lowport, highport+1):
         s = so.socket(so.AF_INET, so.SOCK_STREAM)#utilizzo di AF_INET per connessione iPv4 e SOCK_STREAM per definire che il socket sia di tipo TCP
-        so.setdefaulttimeout(1)  # Timeout di 1 secondo
-        result = s.connect_ex((ip, port))  # Test di connessione alla porta
-        if result == 0:
-            print(f"Porta {port}: Aperta")
+        so.setdefaulttimeout(0.01)  # Timeout di 0.01 secondo
+        result = s.connect_ex((ip, port))  # Test di connessione alla porta(ritorna un errore e non alza l'eccezione in caso di errore)
+        if result == 0: # connect_ex ritorna 0 in caso di connessione riuscita altrimenti qualsiasi altro errore risulta in porta chiusa
+            #print(f"Porta {port}: Aperta")
+            porte_aperte.append(port)
         else:
-            print(f"Porta {port}: Chiusa")
+            porte_chiuse.append(port)
         s.close()
+    return porte_aperte,porte_chiuse
 
-port_scan(target_ip, lowport, highport)
+def crea_tabella(target_ip , porte_aperte, porte_chiuse):
+    tabella1 = PrettyTable(['Porta','Stato'])
+    tabella2 = PrettyTable(['Porta','Stato'])
+
+    for port in porte_aperte:
+        tabella1.add_row([port , 'Aperta'])
+
+    for port in porte_chiuse:
+        tabella2.add_row([port , 'Chiusa'])
+
+    print(f"Risultati per la scansione su {target_ip}: \n")
+    print(tabella1)
+    print(tabella2)
+
+
+verificaSocket(target_ip, lowport, highport)
+crea_tabella(target_ip , porte_aperte , porte_chiuse)
